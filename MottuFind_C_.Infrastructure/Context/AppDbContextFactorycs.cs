840f36b6cd27161f6a6b4bc9f3a37b9865ab8ac4
Gui11epio <guilherme.laiber@gmail.com>
2025-09-08 16:58:26 -0300

    Migrations adicionado

---
+++ MottuFind_C_.Infrastructure/Context/AppDbContextFactorycs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Sprint1_C_.Infrastructure.Data;
+
+namespace MottuFind_C_.Infrastructure.Context
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            
+            var connectionString = Environment.GetEnvironmentVariable("DEFAULT_CONNECTION");
+
+            optionsBuilder.UseOracle(connectionString);
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
